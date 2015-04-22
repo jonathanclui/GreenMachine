b22cf7e940a40f6fa97bd395efbaef54d8e33737
Jonathan Lui <lui.jonathanc@gmail.com>
2015-04-22 08:57:19 -0700

    Added page flow for matching driver and rider

--- app/src/main/java/com/gogreen/greenmachine/DrivingActivity.java
+++ app/src/main/java/com/gogreen/greenmachine/DrivingActivity.java
@@ -1,9 +1,12 @@
 package com.gogreen.greenmachine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.parse.ParseGeoPoint;
 
@@ -15,6 +18,15 @@ public class DrivingActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driving);
+
+        // Set up the handler for the match button click
+        Button matchButton = (Button) findViewById(R.id.driver_match_button);
+        matchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(DrivingActivity.this, MatchingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
--- app/src/main/java/com/gogreen/greenmachine/MatchingActivity.java
+++ app/src/main/java/com/gogreen/greenmachine/MatchingActivity.java
@@ -1,39 +1,24 @@
 package com.gogreen.greenmachine;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import com.parse.ParseGeoPoint;
 
 
-public class RidingActivity extends ActionBarActivity {
-    private ParseGeoPoint geoPoint;
+public class MatchingActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_riding);
-
-        // Set up the handler for the match button click
-        Button matchButton = (Button) findViewById(R.id.rider_match_button);
-        matchButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(RidingActivity.this, MatchingActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_matching);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_riding, menu);
+        getMenuInflater().inflate(R.menu.menu_matching, menu);
         return true;
     }
 

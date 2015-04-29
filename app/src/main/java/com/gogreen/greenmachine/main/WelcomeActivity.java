e8cd082e5b6ad752cc3858c51ed0316aad438938
Jonathan Lui <lui.jonathanc@gmail.com>
2015-04-29 15:42:36 -0700

    Arbaz's changes: added google oauth login

--- app/src/main/java/com/gogreen/greenmachine/main/WelcomeActivity.java
+++ app/src/main/java/com/gogreen/greenmachine/main/WelcomeActivity.java
@@ -1,6 +1,14 @@
 package com.gogreen.greenmachine.main;
 
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,15 +16,46 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.gogreen.greenmachine.R;
+import com.gogreen.greenmachine.main.login.DispatchActivity;
 import com.gogreen.greenmachine.main.login.LoginActivity;
 import com.gogreen.greenmachine.main.login.SignUpActivity;
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 
 public class WelcomeActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+
+    static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
+    static final int REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR = 1001;
+    static final int REQUEST_CODE_RECOVER_FROM_AUTH_ERROR = 1002;
+    private static final String SCOPE = "oauth2:https://www.googleapis.com/auth/userinfo.profile";
+    private String mEmail; // Received from newChooseAccountIntent(); passed to getToken()
+    private ProgressDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +83,16 @@ public class WelcomeActivity extends ActionBarActivity {
                 startActivity(new Intent(WelcomeActivity.this, SignUpActivity.class));
             }
         });
+
+        // Sign up button click handler
+        SignInButton signGoogleButton = (SignInButton) findViewById(R.id.signgoogle_button);
+        signGoogleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Starts an intent for the sign up activity
+                mDialog = new ProgressDialog(WelcomeActivity.this);
+                login();
+            }
+        });
     }
 
     @Override
@@ -57,5 +106,178 @@ public class WelcomeActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login() {
+        pickUserAccount();
+    }
+
+    private void pickUserAccount() {
+        String[] accountTypes = new String[]{"com.google"};
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null, accountTypes, false, null, null, null, null);
+        startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PICK_ACCOUNT) {
+            // Receiving a result from the AccountPicker
+            if (resultCode == RESULT_OK) {
+                mEmail = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+
+                // With the account name acquired, go get the auth token
+                getToken();
+            } else if (resultCode == RESULT_CANCELED) {
+                // The account picker dialog closed without selecting an account.
+                // Notify users that they must pick an account to proceed.
+                Toast.makeText(this, "Pick Account", Toast.LENGTH_SHORT).show();
+            }
+        } else if ((requestCode == REQUEST_CODE_RECOVER_FROM_AUTH_ERROR ||
+                requestCode == REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR)
+                && resultCode == RESULT_OK) {
+            // Receiving a result that follows a GoogleAuthException, try auth again
+            getToken();
+        }
+    }
+
+    private void getToken() {
+        if (mEmail == null) {
+            pickUserAccount();
+        } else {
+            if (isDeviceOnline()) {
+                new getTokenTask(WelcomeActivity.this, mEmail, SCOPE).execute();
+            } else {
+                Toast.makeText(this, "Not online", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    /**
+     * This method is a hook for background threads and async tasks that need to
+     * provide the user a response UI when an exception occurs.
+     */
+    public void handleException(final Exception e) {
+        // Because this call comes from the AsyncTask, we must ensure that the following
+        // code instead executes on the UI thread.
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (e instanceof GooglePlayServicesAvailabilityException) {
+                    // The Google Play services APK is old, disabled, or not present.
+                    // Show a dialog created by Google Play services that allows
+                    // the user to update the APK
+                    int statusCode = ((GooglePlayServicesAvailabilityException) e).getConnectionStatusCode();
+                    Dialog dialog = GooglePlayServicesUtil.getErrorDialog(statusCode, WelcomeActivity.this, REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
+                    dialog.show();
+                } else if (e instanceof UserRecoverableAuthException) {
+                    // Unable to authenticate, such as when the user has not yet granted
+                    // the app access to the account, but the user can fix this.
+                    // Forward the user to an activity in Google Play services.
+                    Intent intent = ((UserRecoverableAuthException) e).getIntent();
+                    startActivityForResult(intent, REQUEST_CODE_RECOVER_FROM_PLAY_SERVICES_ERROR);
+                }
+            }
+        });
+    }
+
+    public boolean isDeviceOnline() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public class getTokenTask extends AsyncTask {
+        Activity mActivity;
+        String mScope;
+        String mEmail;
+
+        getTokenTask(Activity activity, String name, String scope) {
+            this.mActivity = activity;
+            this.mScope = scope;
+            this.mEmail = name;
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
+                String token = fetchToken();
+                String url = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + token;
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response = httpclient.execute(new HttpGet(url));
+                StatusLine statusLine = response.getStatusLine();
+                if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    String responseString = out.toString();
+                    out.close();
+                    JSONObject jObject = new JSONObject(responseString);
+                    String username = jObject.getString("name");
+                    ParseUser user = new ParseUser();
+                    user.setUsername(username);
+                    user.setEmail(this.mEmail);
+                    user.setPassword(token);
+                    user.put("profileComplete", false);
+
+                    // Call the parseobjects signup method
+                    user.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            //dialog.dismiss();
+                            if (e != null) {
+                                // Show the error message
+                                Toast.makeText(WelcomeActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            } else {
+                                // Start an intent for the dispatch activity
+                                Intent intent = new Intent(WelcomeActivity.this, DispatchActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+
+                } else {
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+                //Preferences.saveString(Constants.KEY_BLOGGER_TOKEN, token);
+            } catch (IOException e) {
+                // The fetchToken() method handles Google-specific exceptions,
+                // so this indicates something went wrong at a higher level.
+                // TIP: Check for network connectivity before starting the AsyncTask.
+            } catch (JSONException e) {
+                // The fetchToken() method handles Google-specific exceptions,
+                // so this indicates something went wrong at a higher level.
+                // TIP: Check for network connectivity before starting the AsyncTask.
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+        /**
+         * Gets an authentication token from Google and handles any
+         * GoogleAuthException that may occur.
+         */
+        protected String fetchToken() throws IOException {
+            try {
+                return GoogleAuthUtil.getToken(mActivity, mEmail, mScope);
+            } catch (UserRecoverableAuthException userRecoverableException) {
+                // GooglePlayServices.apk is either old, disabled, or not present
+                // so we need to show the user some UI in the activity to recover.
+                ((WelcomeActivity) mActivity).handleException(userRecoverableException);
+
+            } catch (GoogleAuthException fatalException) {
+                // Some other type of unrecoverable exception has occurred.
+                // Report and log the error as appropriate.
+            } finally {
+                String dl = "";
+            }
+            return null;
+        }
     }
 }

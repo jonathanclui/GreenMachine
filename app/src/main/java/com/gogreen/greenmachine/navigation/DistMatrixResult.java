9d547f3385b0d205732db4a9c236b96532f682dc
Arbaz Khan <arbazkhan002@gmail.com>
2015-04-30 11:31:10 -0700

    distmatrix in

---
+++ app/src/main/java/com/gogreen/greenmachine/navigation/DistMatrixResult.java
@@ -0,0 +1,28 @@
+package com.gogreen.greenmachine.navigation;
+
+/**
+ * Created by arbkhan on 4/30/2015.
+ */
+public static class DistMatrixResult {
+
+}
+
+public static class Address {
+
+}
+
+public static class DistMatrixRow {
+
+}
+
+public static class DistMatrixElement {
+
+}
+
+public static class DistMatrixDistance {
+
+}
+
+public static class DistMatrixDuration {
+
+}
